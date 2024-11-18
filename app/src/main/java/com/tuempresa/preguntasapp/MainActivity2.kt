248cf1dd9b33c3bb4e93922fb191deac9e428127
Francisco José Bermúdez Hermoso <franciscojose.bermudez@vass.es>
2024-11-18 08:09:44 +0100

    Nueva activity para clonar las vistas en compose

---
+++ app/src/main/java/com/tuempresa/preguntasapp/MainActivity2.kt
@@ -0,0 +1,10 @@
+package com.tuempresa.preguntasapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
